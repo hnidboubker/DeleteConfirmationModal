dd2ff5d9e68276b2f15250a6513a5f03cef0f68d
NID BOUBKER Houssine <nidboubkerhoussine@live.fr>
2025-01-04 06:03:17 +0100

    Commit

---
+++ Install.ps1
@@ -0,0 +1,17 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+# Copier le fichier _DeleteConfirmation.cshtml
+$sourcePathHtml = Join-Path $installPath "Views\Shared\_DeleteConfirmation.cshtml"
+$destinationPathHtml = Join-Path $project.FullPath "Views\Shared\_DeleteConfirmation.cshtml"
+
+if (!(Test-Path $destinationPathHtml)) {
+    Copy-Item $sourcePathHtml -Destination $destinationPathHtml
+}
+
+# Copier le fichier deleteConfirmation.js
+$sourcePathJs = Join-Path $installPath "tools\Scripts\deleteConfirmation.js"
+$destinationPathJs = Join-Path $project.FullPath "wwwroot\js\deleteConfirmation.js"
+
+if (!(Test-Path $destinationPathJs)) {
+    Copy-Item $sourcePathJs -Destination $destinationPathJs
+}
---
+++ Tools/Scripts/deleteConfirmation.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function() {
+        var form;
+$('body').on('click', '.btn-delete', function(e) {
+    e.preventDefault();
+form = $(this).closest('form');
+$('#deleteConfirmationModal').modal('show');
+        });
+
+$('#confirmDeleteButton').click(function() {
+    form.submit();
+        });
+    });
